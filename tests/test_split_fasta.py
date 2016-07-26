1f803b9719ca919016b4b5bfdc3fa75395984770
J.F.J. Laros <J.F.J.Laros@lumc.nl>
2016-07-26 20:39:43 +0200

    More sensible storage of file handles.

--- tests/test_split_fasta.py
+++ tests/test_split_fasta.py
@@ -8,8 +8,9 @@ from shared import FakeOpen, md5_check
 
 class TestSplitFasta(object):
     def setup(self):
-        self._fake_open = FakeOpen()
+        fake_open = FakeOpen()
+        self._handles = fake_open.handles
         split_fasta.open = self._fake_open.open
 
     def _md5_check(self, fileno, md5sum):
-        return md5check(self._fake_open.handles[fileno].getvalue(), md5sum)
+        return md5check(self._handles[fileno].getvalue(), md5sum)
