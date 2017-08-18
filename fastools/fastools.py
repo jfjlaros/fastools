84d2897be2c8cf1bc268e2e61d64e299b0ce6944
dimitra <D.Zafeiropoulou@lumc.nl>
2017-08-18 16:24:19 +0200

    add some spacing

--- fastools/fastools.py
+++ fastools/fastools.py
@@ -46,8 +46,7 @@ def _write_seq(handle, seq, name, file_format='fasta'):
 
 
 def _edits_read(handle):
-    """
-    Parse a FASTA file that contains edits.
+    """Parse a FASTA file that contains edits.
 
     :arg stream handle: Open readable handle to a FASTA file.
     :returns dict: A list of edits (ranges and replacements) per chromosome.
@@ -69,6 +68,7 @@ def _find_motif(record, motif):
 
     :arg SeqRecord record: Seq object which will be searched.
     :arg str motif: The sequence to be found.
+
     :returns generator(tuple(int, int)): tuple of start and end of matches in record.
     """
     regex = re.compile(motif.strip(), re.IGNORECASE)
@@ -114,12 +114,12 @@ class UMIExtractor(object):
     def __init__(
             self, input_handle, output_handle, location, bases_start,
             bases_end, file_format):
-
         """Configure UMI extractor.
 
         :arg stream input_handle:  readable handle to a fastq file
         :arg stream output_handle: writable handle to an fastq file
-        :arg str location: where the umis is located on the read: can be start,end,both_ends, index2
+        :arg str location: where the umis is located on the read: can
+            be start,end,both_ends, index2
         :arg int bases_start: number of bases in the start where umi is located
         :arg int bases_end: number of bases in the end where umi is located
         :arg str file_format: file format
@@ -134,9 +134,8 @@ class UMIExtractor(object):
         self._value_pair_checker()
 
     def extractor(self):
-        """
-          Extract the UMI form the read, fix the sequence and the quality,
-          add the umi to the read header and write a new fastq file
+        """Extract the UMI form the read, fix the sequence and the quality,
+           add the umi to the read header and write a new fastq file
         """
         record_iter = SeqIO.parse(
             open(self.input_handle.name), self.file_format)
@@ -168,30 +167,36 @@ class UMIExtractor(object):
                 SeqIO.write(record, self.output_handle, self.file_format)
 
     def append_umi_identifier_to_read_header(self, description, identifier):
-        """
-        For a given read append the umi identifier to the read
+        """For a given read append the umi identifier to the read
 
         :arg str identifier: umi sequence
         :arg str description: read description
-        :return str: fixed read description with umi identifier included
+
+        :returns str: fixed read description with umi identifier included
         """
         header_format = guess_header_format(self.input_handle)
 
         if header_format == 'x':
             description_splitted_part1, description_splitted_part2 = description.split(' ')
-            return '{}_{} {}'.format(description_splitted_part1, identifier, description_splitted_part2)
+            return '{}_{} {}'.format(
+                description_splitted_part1,
+                identifier,
+                description_splitted_part2)
+
         elif header_format == 'normal':
             description_splitted_part1, description_splitted_part2 = description.split('#')
-            return '{}_{}#{}'.format(description_splitted_part1, identifier, description_splitted_part2)
+            return '{}_{}#{}'.format(
+                description_splitted_part1,
+                identifier,
+                description_splitted_part2)
         else:
             raise RuntimeError('Not Valid Header Format')
 
     def extract_from_index2(self,record):
-        """
-        Extract the UMI sequence from index2
+        """Extract the UMI sequence from index2
 
         :arg SeqIO record: sequence record
-        :return str: extracted UMI sequence
+        :returns str: extracted UMI sequence
         """
         header_format = guess_header_format(self.input_handle)
         if header_format == 'x':
@@ -213,10 +218,10 @@ class UMIExtractor(object):
             return RuntimeError('Uknown Header Format')
 
     def extract_from_start(self, record):
-        """
-        Extract UMI from the start of the read
+        """Extract UMI from the start of the read
+
         :arg SeqIO record: sequence record
-        :return str,str,str: extracted UMI sequence, fixed sequence, fixed quality
+        :returns str,str,str: extracted UMI sequence, fixed sequence, fixed quality
         """
         seq_length = len(record.seq)
         return (
@@ -225,10 +230,10 @@ class UMIExtractor(object):
             record.letter_annotations['phred_quality'][self.bases_start:seq_length])
 
     def extract_from_end(self, record):
-        """
-        Extract UMI from the end of the read
+        """Extract UMI from the end of the read
+
         :arg SeqIO record: sequence record
-        :return str,str,str: extracted UMI sequence, fixed sequence, fixed quality
+        :returns str,str,str: extracted UMI sequence, fixed sequence, fixed quality
         """
         seq_length = len(record.seq)
         seq_fixed_end = seq_length - self.bases_end
@@ -237,10 +242,10 @@ class UMIExtractor(object):
             record.letter_annotations['phred_quality'][0:seq_fixed_end])
 
     def extract_from_both_ends(self, record):
-        """
-        Extract UMI from both ends of the read
+        """Extract UMI from both ends of the read
+
         :arg SeqIO record: sequence record
-        :return str, str, str: extracted UMI sequence, fixed sequence, fixed quality
+        :returns str, str, str: extracted UMI sequence, fixed sequence, fixed quality
         """
         seq_length = len(record.seq)
         seq_fixed_end = seq_length - self.bases_end
@@ -250,8 +255,7 @@ class UMIExtractor(object):
             record.letter_annotations['phred_quality'][self.bases_start:seq_fixed_end])
 
     def _value_pair_checker(self):
-        """
-        Checks for missing value information for different umi locations
+        """Checks for missing value information for different umi locations
         """
         if self.location == 'start' and self.bases_start is None:
             raise Exception('Invalid Value-Pair: {}-{}'.format(
@@ -268,8 +272,7 @@ class UMIExtractor(object):
 
 
 class UMIAppender(object):
-    """
-    Configure UMI appender.
+    """Configure UMI appender.
 
     :arg stream input_handle:  readable handle to a fastq file
     :arg stream output_handle: writable handle to an fastq file
@@ -282,11 +285,10 @@ class UMIAppender(object):
         self.file_format = file_format
 
     def get_umi_identifier_from_read_header(self, description):
-        """
-        Extract UMI identifier from read description
+        """Extract UMI identifier from read description
 
-        :str description: read description
-        :return str: UMI sequence
+        :arg str description: read description
+        :returns str: UMI sequence
         """
         header_format = guess_header_format(self.input_handle)
 
@@ -298,8 +300,7 @@ class UMIAppender(object):
             raise RuntimeError('Not Valid Header Format')
 
     def umi_appender(self):
-        """
-        Append the UMI at the start of the read and mock qualities
+        """Append the UMI at the start of the read and mock qualities
         """
         record_iter = SeqIO.parse(
             open(self.input_handle.name), self.file_format)
