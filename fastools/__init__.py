8d1ffc9820ad1c2b4e3b5754c60be40ba65a25b8
Jeroen F.J. Laros <jlaros@fixedpoint.nl>
2026-06-07 16:36:13 +0200

    Cleaning.

--- fastools/__init__.py
+++ fastools/__init__.py
@@ -28,15 +28,3 @@ _description = _extract('Summary')
 _copyright = f'Copyright (c) {_year} by {_author} <{_email}>'
 _url = _extract('Project-URL', r', ', 1)
 _info = f'{_project} version {_version}\n\n{_copyright}\nHomepage: {_url}'
-
-
-#_copyright_notice = 'Copyright (c) {} <{}>'.format(
-#    _get_metadata('Author'), _get_metadata('Author-email'))
-#
-#usage = [_get_metadata('Summary'), _copyright_notice]
-#
-#
-#def version(name):
-#    return '{} version {}\n\n{}\nHomepage: {}'.format(
-#        _get_metadata('Name'), _get_metadata('Version'), _copyright_notice,
-#        _get_metadata('Home-page'))
